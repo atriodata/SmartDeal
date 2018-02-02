df259c85d7d26caa77c5719f512e779c643cf64c
Atrio <info@atrio.co.in>
2018-02-02 16:19:04 +0530

    search icon added

--- app/src/main/java/com/atrio/smartdeal/MainActivity.kt
+++ app/src/main/java/com/atrio/smartdeal/MainActivity.kt
@@ -3,7 +3,10 @@ package com.atrio.smartdeal
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import com.atrio.smartdeal.model.ProductList
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,26 +15,35 @@ import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    var searchItem:String?=null
     val BASE_URL="https://price-api.datayuge.com/"
     val API_KEY = "u6SMILRT5RfgvgnXqQlX3qbrd67ahnZdvhi"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("HelloApp","Welcome")
 
-        getProductSearch()
+        serch_item.setOnTouchListener(object : View.OnTouchListener{
+            override fun onTouch(v: View, event: MotionEvent): Boolean {
+                if (event.action==MotionEvent.ACTION_UP){
+                    Log.i("HelloApp","Welcome")
+                    searchItem=serch_item.text.toString()
+                    getProductSearch()
+                }
+                return false }
+
+        })
 
 
     }
 
     private fun getProductSearch() {
 
-        var searchItem="Redmi"
+
 
         val retrofitobject= Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
         val apiService=retrofitobject.create(ApiInterface::class.java)
-        val call=apiService.getProductList(searchItem,API_KEY)
+        val call=apiService.getProductList(searchItem!!,API_KEY)
         call.enqueue(object :Callback<ProductList>{
             override fun onFailure(call: Call<ProductList>, t: Throwable) {
                 Log.i("goterror",t.localizedMessage)
